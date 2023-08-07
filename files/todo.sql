53cd48b932c48d1cf836543d51cfd7452df76822
Gramirez23 <gpramirezecheverria@gmail.com>
2023-08-06 21:47:26 -0300

    Correcion de errores y restauracion de base dbserver
    Se corrigieron errores generales en archivos de configuracion
    y se crearon tareas para restaurar la base de datos todo.

---
+++ files/todo.sql
@@ -0,0 +1,21 @@
+USE todo;
+
+CREATE TABLE `users` (
+`id` int(3) NOT NULL AUTO_INCREMENT,
+`first_name` varchar(20) DEFAULT NULL,
+`last_name` varchar(20) DEFAULT NULL,
+`username` varchar(250) DEFAULT NULL,
+`password` varchar(20) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `todos` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`description` varchar(255) DEFAULT NULL,
+`is_done` bit(1) NOT NULL,
+`target_date` datetime(6) DEFAULT NULL,
+`username` varchar(255) DEFAULT NULL,
+`title` varchar(255) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
